850c748cecbae74835dcb86029979846912b5b75
Jaron <jaronvwell@gmail.com>
2024-05-09 21:23:33 +0200

    Added animation stop when talking

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rigidBody;
@@ -10,15 +11,22 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        rigidBody.velocity = new Vector2(moveInput.x * walkSpeed, moveInput.y * walkSpeed);
-
         playerAnimator.SetFloat("Horizontal", moveInput.x);
         playerAnimator.SetFloat("Vertical", moveInput.y);
+
+        if (playerAnimator.GetBool("IsTalking"))
+        {
+            return;
+        }
+
+        rigidBody.velocity = new Vector2(moveInput.x * walkSpeed, moveInput.y * walkSpeed);
+
 
         if (moveInput.x == 0 && moveInput.y == 0)
         {
             playerAnimator.SetBool("IsMoving", false);
-        } else if (moveInput.x != 0 || moveInput.y != 0)
+        }
+        else if (moveInput.x != 0 || moveInput.y != 0)
         {
             playerAnimator.SetBool("IsMoving", true);
         }
@@ -27,14 +35,12 @@ public class PlayerController : MonoBehaviour
         {
             playerAnimator.SetFloat("Last Horizontal", moveInput.x);
             playerAnimator.SetFloat("Last Vertical", 0);
-
         }
         else if (moveInput.y == 1 || moveInput.y == -1)
         {
             playerAnimator.SetFloat("Last Vertical", moveInput.y);
             playerAnimator.SetFloat("Last Horizontal", 0);
         }
-        
     }
 
     public void OnMove(InputAction.CallbackContext context)
